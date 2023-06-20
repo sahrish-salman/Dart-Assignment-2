3e655c7d8e20b450eca440de9ec9d35fa19b1a6a
sahrish-salman <sahrish.salman@gmail.com>
2023-06-20 20:23:05 +0500

    first commit

---
+++ main.dart
@@ -0,0 +1,17 @@
+// Create Linkedin Profile
+// Create Github Profile
+// Share both link as assignment.
+
+void main() {
+  String linkedinProfile =
+      "https://www.linkedin.com/in/sahrish-khan-abcd12345/";
+  String githubProfile = "https://github.com/sahrish-salman/";
+
+  if (linkedinProfile ==
+          "https://www.linkedin.com/in/sahrish-khan-abcd12345/" &&
+      githubProfile == "https://github.com/sahrish-salman/") {
+    print('My Linkedin and Github Profile Link');
+  } else {
+    print('Something Wrong');
+  }
+}
